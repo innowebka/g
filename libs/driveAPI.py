372bc83af09c111eb074be60ee4907cecf4e5f7f
innowebka <57298213+innowebka@users.noreply.github.com>
2020-05-11 14:36:04 +0530

    Add files via upload

---
+++ libs/driveAPI.py
@@ -0,0 +1,189 @@
+#!/usr/bin/python3
+
+import os.path, pickle, zipfile
+
+from apiclient.http import MediaIoBaseDownload
+from apiclient.http import MediaIoBaseUpload
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from io import BytesIO
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+#Creates Service object to allow interaction with Google Drive
+def get_service():
+	"""Shows basic usage of the Drive v3 API.
+	Prints the names and ids of the first 10 files the user has access to.
+	"""
+	creds = None
+	# The file token.pickle stores the user's access and refresh tokens, and is
+	# created automatically when the authorization flow completes for the first
+	# time.
+	if os.path.exists('token.pickle'):
+		with open('token.pickle', 'rb') as token:
+			creds = pickle.load(token)
+	# If there are no (valid) credentials available, let the user log in.
+	if not creds or not creds.valid:
+		if creds and creds.expired and creds.refresh_token:
+			creds.refresh(Request())
+		else:
+			flow = InstalledAppFlow.from_client_secrets_file(
+				'credentials.json', SCOPES)
+			creds = flow.run_local_server()
+		# Save the credentials for the next run
+		with open('token.pickle', 'wb') as token:
+			pickle.dump(creds, token)
+
+	service = build('drive', 'v3', credentials=creds)
+	return service
+
+#Creates the InfiniDrive root folder and returns its ID.
+def create_root_folder(service):
+	root_meta = {
+		'name': "InfiniDrive Root",
+		'mimeType': 'application/vnd.google-apps.folder',
+		'properties': {'infinidriveRoot': 'true'},
+		'parents': []
+	}
+	root_folder = service.files().create(body=root_meta,
+		fields='id').execute()
+
+	# Hide the root folder folder
+	service.files().update(fileId=root_folder['id'], removeParents='root').execute()
+
+	return root_folder
+
+#Gets the ID of the InfiniDrive root folder.
+def get_root_folder_id(service):
+	results = service.files().list(
+		q="properties has {key='infinidriveRoot' and value='true'} and trashed=false",
+		pageSize=1,
+		fields="nextPageToken, files(id, name, properties)").execute()
+	folders = results.get('files', [])
+
+	if not folders:
+		return create_root_folder(service)['id']
+	else:
+		return folders[0]['id']
+
+#Creates a folder and returns its ID
+def create_folder(service, file_path):
+	folder_metadata = {
+	'name': file_path,
+	'mimeType': 'application/vnd.google-apps.folder',
+	'parents': [get_root_folder_id(service)]
+	}
+	folder = service.files().create(body=folder_metadata, fields= 'id').execute()
+
+	return folder.get('id')
+
+#Stores a file into a folder
+def store_doc(service, folderId, file_name, crc32, file_path):
+	file_metadata = {
+	'name': file_name,
+	'mimeType': 'application/vnd.google-apps.document',
+	'parents': [folderId],
+	'properties': {'crc32': str(crc32)}
+	}
+	media = MediaIoBaseUpload(file_path, mimetype='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+	service.files().create(body=file_metadata,
+									media_body=media,
+									fields = 'id').execute()
+
+#Returns folder id and service object for document insertion into the folder
+def begin_storage(file_path):
+	service = get_service()
+	folderId = create_folder(service, file_path)
+	return service, folderId
+
+#Lists folders and their IDs (excluding folders in Trash)
+def list_files(service):
+	query = "(mimeType='application/vnd.google-apps.folder') and (trashed=False) and ('" + get_root_folder_id(service) + "' in parents)"
+	page_token = None
+	filesList = list()
+	while True:
+		param = {}
+		
+		if page_token:
+			param['pageToken'] = page_token
+		
+		results = service.files().list(q=query, fields="nextPageToken, files(id, name)", **param).execute()
+		filesList += results.get('files', [])
+		
+		page_token = results.get('nextPageToken')
+		if not page_token:
+			break
+	
+	filesList = [[folder.get('name'), folder.get('id')] for folder in filesList]
+	filesList.sort()
+	return filesList
+
+# Deletes the file with the given ID.
+def delete_file(service, file_id):
+	service.files().delete(fileId=file_id).execute()
+
+# Renames the file with the given ID.
+def rename_file(service, file_id, new_name):
+	file = {'name': new_name}
+
+	# Rename the file.
+	service.files().update(
+		fileId=file_id,
+		body=file,
+		fields='name').execute()
+
+# Returns a list of files in a folder with the given ID
+def get_files_list_from_folder(service, folderId):
+	query = "'" +folderId + "' in parents"
+	page_token = None
+	files = list()
+	while True:
+		param = {}
+
+		if page_token:
+			param['pageToken'] = page_token
+
+		results = service.files().list(q=query, fields='nextPageToken, files(id, name, properties)', **param).execute()
+		files += results.get('files', []) #grabs all of the files from the folder
+
+		page_token = results.get('nextPageToken')
+		if not page_token:
+			break
+	return files
+
+# Returns a list of files in a folder with the given ID with the given name
+def get_files_with_name_from_folder(service, folderId, name):
+	query = "'" + folderId + "' in parents and name='" + str(name) + "'"
+	page_token = None
+	files = list()
+	while True:
+		param = {}
+
+		if page_token:
+			param['pageToken'] = page_token
+
+		results = service.files().list(q=query, fields='nextPageToken, files(id, name, properties)', **param).execute()
+		files += results.get('files', []) #grabs all of the files from the folder
+
+		page_token = results.get('nextPageToken')
+		if not page_token:
+			break
+	return files
+
+# Returns the bytes from an image in a document
+def get_image_bytes_from_doc(service, file):
+	# Download file to memory
+	request = service.files().export_media(fileId=file['id'], mimeType='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+	fh = BytesIO()
+	downloader = MediaIoBaseDownload(fh, request)
+	done = False
+	while done is False:
+		status, done = downloader.next_chunk()
+
+	# Extract image from file and return the image's bytes
+	zipRef = zipfile.ZipFile(fh, 'r')
+	imgBytes = zipRef.read('word/media/image1.png')
+	zipRef.close()
+	return BytesIO(imgBytes)
---
+++ libs/uploadHandler.py
@@ -0,0 +1,45 @@
+from binascii import crc32
+from docx import Document
+from io import BytesIO
+from PIL import Image
+
+# Handles uploading of a fragment of data to Google Drive.
+def handle_upload_fragment(driveAPI, fileBytes, driveConnect, dirId, docNum, failedFragmentsSet, debug_log):
+	# Add a "spacer byte" at the end to indciate end of data and start of padding.
+	fileBytes += bytes([255])
+
+	# Generate a new Word document.
+	doc = Document()
+
+	# Pad the fragment with enough null bytes to reach the requirements for the image dimensions.
+	fileBytes += bytes(10224000 - len(fileBytes))
+	
+	# Calculate CRC32 hash for fileBytes
+	hash = hex(crc32(fileBytes))
+
+	# Generate and save a temporary PNG in memory.
+	img = Image.frombytes('RGB', (2000, 1704), fileBytes)
+	mem_img = BytesIO()
+	img.save(mem_img, 'PNG')
+
+	# Add temporary PNG to the Word document.
+	doc.add_picture(mem_img)
+
+	# Save the generated Word document.
+	mem_doc = BytesIO()
+	doc.save(mem_doc)
+
+	# Upload Word document to Google Drive
+	while True:
+		try:
+			driveAPI.store_doc(driveConnect, dirId, str(docNum) + ".docx", hash, mem_doc)
+		except Exception as e:
+			# If a fragment upload failure occurs, log the incident, add docNum to failedFragmentsSet,
+			# and try again.
+			debug_log.write("----------------------------------------\n")
+			debug_log.write("Fragment upload failure. Fragment number is " + str(docNum) + ".\n")
+			debug_log.write("Error:\n")
+			debug_log.write(str(e) + "\n")
+			failedFragmentsSet.add(docNum)
+			continue
+		break
